c8f370e21d4405604a92c4b0fa457d676ace5de3
Aiswaryakrishnamurthi <aiswarya.krishnamurthi@gmail.com>
2024-12-28 14:43:06 +0530

    Update Test1.java

--- javaInterface/javaInterface/src/javaInterface/Test1.java
+++ javaInterface/javaInterface/src/javaInterface/Test1.java
@@ -3,6 +3,8 @@ package javaInterface;
 //Using Lambda Expressions
 /*If the interface is a functional interface (an interface with only one abstract method),
 you can use a lambda expression to provide the implementation.*/
+
+// also with anonymous class
 @FunctionalInterface
  interface Animal1 {
     void eat(); // Functional interface
@@ -12,5 +14,17 @@ public class Test1 {
     public static void main(String[] args) {
         Animal1 myAnimal = () -> System.out.println("Lambda Animal is eating.");
         myAnimal.eat();
+        
+        Animal1 myAnimal1 = new Animal1() {
+            @Override
+            public void eat() {
+                System.out.println("Anonymous Animal is eating.");
+            }
+
+         
+        };
+        
+        myAnimal1.eat();
+      
     }
 }
